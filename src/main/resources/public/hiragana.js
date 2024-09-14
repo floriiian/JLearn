54141eaeca2ba5dfb74f5cc0e8e6f5f09d7adc95
Florian S. <112857696+floriiian@users.noreply.github.com>
2024-09-15 01:48:53 +0200

    Add files via upload

--- src/main/resources/public/hiragana.js
+++ src/main/resources/public/hiragana.js
@@ -40,6 +40,8 @@ const cardsContainer = document.getElementById("cardsContainer");
 const streakAmount = document.getElementById("streakAmount");
 const progressBar = document.getElementById("progressBar");
 
+const checkButton = document.getElementById("checkButton");
+
 function shakeElement(element) {
     element.classList.add('shake');
     setTimeout(() => {
@@ -97,6 +99,7 @@ function loadNextCard() {
 
     setProgressBarWidth(100 * current / total)
     fadeElement(cardsContainer)
+    switchCheckButton(false);
 
     if (remainingHiragana.length > 0) {
         currentCardIndex = Math.floor(Math.random() * remainingHiragana.length);
@@ -112,52 +115,79 @@ function loadNextCard() {
     }
 }
 
-inputField.addEventListener("keypress", function(event) {
-
-    const userAnswer = inputField.value.toLowerCase().trim();
-
-     if(event.key === "Enter" && userAnswer.length > 0) {
-
-         const correctAnswer = remainingHiragana[currentCardIndex]?.romaji;
-         const currentHiragana = remainingHiragana[currentCardIndex].char;
-
-        let isAnswerCorrect = userAnswer === correctAnswer;
-
-         for (let i = 0; i < doubleRomaji.length; i++) {
-             if (doubleRomaji[i].hiragana === currentHiragana) {
-                 console.log("Is here");
-                  if(userAnswer === doubleRomaji[i].hepburn || userAnswer === doubleRomaji[i].kunrei){
-                      console.log("Is true");
-                      isAnswerCorrect = true;
-                      break;
-                  }
-             }
-         }
-
-        if (isAnswerCorrect) {
-
-            const correctSound = new Audio('sounds/correct' + Math.floor(Math.random() * 4) + ".mp3");
-            correctSound.play();
-
-            const endTime = new Date();
-            const timeTaken = Math.round((endTime - startTime) / 1000); // T
-
-            totalTime += timeTaken;
-            remainingHiragana.splice(currentCardIndex, 1);
-            currentStreak++;
-            loadNextCard();
-
-        } else {
-            shakeElement(cardsContainer);
-            currentStreak = 0;
-            inputField.classList.add("wrong");
-        }
-        if(streakAmount > 0){
-            shakeElement(streakAmount);
-        }
-        streakAmount.textContent = currentStreak;
+function switchCheckButton(enable = true) {
+    if(enable){
+        checkButton.style.backgroundColor = "#7a79ed";
+        checkButton.style.color = "#1b1a38";
+        checkButton.style.pointerEvents = "auto";
     }
-});
+    else{
+        checkButton.style.backgroundColor = "#37444d";
+        checkButton.style.boxShadow = "0 0px 0 #37444d";
+        checkButton.style.color = "#455560";
+        checkButton.style.pointerEvents = "none";
+    }
+}
+
+inputField.addEventListener("keyup", function(event) {
+
+    if(inputField.value.length > 0){
+        switchCheckButton(false);
+        if(event.key === "Enter") {
+            handleAnswer();
+        }
+        switchCheckButton();
+    }
+    else{
+        checkButton.style.backgroundColor = "#37444d";
+        checkButton.style.boxShadow = "0 0px 0 #37444d";
+        checkButton.style.color = "#455560";
+        checkButton.style.pointerEvents = "none";
+    }
+})
+
+function handleAnswer(){
+
+    const userAnswer = inputField.value.toLowerCase().trim()
+    const correctAnswer = remainingHiragana[currentCardIndex]?.romaji;
+    const currentHiragana = remainingHiragana[currentCardIndex].char;
+
+    let isAnswerCorrect = userAnswer === correctAnswer || currentHiragana === "じ" && (userAnswer === "ji" || userAnswer === "zi");
+
+    for (let i = 0; i < doubleRomaji.length; i++) {
+        if (doubleRomaji[i].hiragana === currentHiragana) {
+            console.log("Is here");
+            if(userAnswer === doubleRomaji[i].hepburn || userAnswer === doubleRomaji[i].kunrei){
+                console.log("Is true");
+                isAnswerCorrect = true;
+                break;
+            }
+        }
+    }
+
+    if (isAnswerCorrect) {
+
+        const correctSound = new Audio('sounds/correct' + Math.floor(Math.random() * 4) + ".mp3");
+        correctSound.play();
+
+        const endTime = new Date();
+        const timeTaken = Math.round((endTime - startTime) / 1000); // T
+
+        totalTime += timeTaken;
+        remainingHiragana.splice(currentCardIndex, 1);
+        currentStreak++;
+        loadNextCard();
+
+    } else {
+        shakeElement(cardsContainer);
+        currentStreak = 0;
+        inputField.classList.add("wrong");
+    }
+    if(streakAmount > 0){
+        shakeElement(streakAmount);
+    }
+    streakAmount.textContent = currentStreak;
+}
 
 function playPronunciation() {
     const voices = speechSynthesis.getVoices();
