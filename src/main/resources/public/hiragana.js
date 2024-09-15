2befe4d464ba6b65968c21730469c1e8358bbf74
Florian S. <112857696+floriiian@users.noreply.github.com>
2024-09-15 13:46:03 +0200

    Add files via upload

--- src/main/resources/public/hiragana.js
+++ src/main/resources/public/hiragana.js
@@ -1,30 +1,51 @@
-const availableHiragana=
-    [{char:"あ",romaji:"a"},{char:"い",romaji:"i"},{char:"う",romaji:"u"},{char:"え",romaji:"e"},{char:"お",romaji:"o"},{char:"か",romaji:"ka"},{char:"き",romaji:"ki"},{char:"く",romaji:"ku"},{char:"け",romaji:"ke"},{char:"こ",romaji:"ko"},{char:"さ",romaji:"sa"},{char:"し",romaji:"shi"},{char:"す",romaji:"su"},{char:"せ",romaji:"se"},{char:"そ",romaji:"so"},{char:"た",romaji:"ta"},{char:"ち",romaji:"chi"},{char:"つ",romaji:"tsu"},{char:"て",romaji:"te"},{char:"と",romaji:"to"},{char:"な",romaji:"na"},{char:"に",romaji:"ni"},{char:"ぬ",romaji:"nu"},{char:"ね",romaji:"ne"},{char:"の",romaji:"no"},{char:"は",romaji:"ha"},{char:"ひ",romaji:"hi"},{char:"ふ",romaji:"fu"},{char:"へ",romaji:"he"},{char:"ほ",romaji:"ho"},{char:"ま",romaji:"ma"},{char:"み",romaji:"mi"},{char:"む",romaji:"mu"},{char:"め",romaji:"me"},{char:"も",romaji:"mo"},{char:"や",romaji:"ya"},{char:"ゆ",romaji:"yu"},{char:"よ",romaji:"yo"},{char:"ら",romaji:"ra"},{char:"り",romaji:"ri"},{char:"る",romaji:"ru"},{char:"れ",romaji:"re"},{char:"ろ",romaji:"ro"},{char:"わ",romaji:"wa"},{char:"を",romaji:"wo"},{char:"ん",romaji:"n"},{char:"が",romaji:"ga"},{char:"ぎ",romaji:"gi"},{char:"ぐ",romaji:"gu"},{char:"げ",romaji:"ge"},{char:"ご",romaji:"go"},{char:"ざ",romaji:"za"},{char:"じ",romaji:"ji"},{char:"ず",romaji:"zu"},{char:"ぜ",romaji:"ze"},{char:"ぞ",romaji:"zo"},{char:"だ",romaji:"da"},{char:"ぢ",romaji:"zi"},{char:"づ",romaji:"zu"},{char:"で",romaji:"de"},{char:"ど",romaji:"do"},{char:"ば",romaji:"ba"},{char:"び",romaji:"bi"},{char:"ぶ",romaji:"bu"},{char:"べ",romaji:"be"},{char:"ぼ",romaji:"bo"},{char:"ぱ",romaji:"pa"},{char:"ぴ",romaji:"pi"},{char:"ぷ",romaji:"pu"},{char:"ぺ",romaji:"pe"},{char:"ぽ",romaji:"po"},{char:"きゃ",romaji:"kya"},{char:"きゅ",romaji:"kyu"},{char:"きょ",romaji:"kyo"},{char:"しゃ",romaji:"sya"},{char:"しゅ",romaji:"syu"},{char:"しょ",romaji:"syo"},{char:"ちゃ",romaji:"cha"},{char:"ちゅ",romaji:"chu"},{char:"ちょ",romaji:"cho"},{char:"にゃ",romaji:"nya"},{char:"にゅ",romaji:"nyu"},{char:"にょ",romaji:"nyo"},{char:"ひゃ",romaji:"hya"},{char:"ひゅ",romaji:"hyu"},{char:"ひょ",romaji:"hyo"},{char:"みゃ",romaji:"mya"},{char:"みゅ",romaji:"myu"},{char:"みょ",romaji:"myo"},{char:"りゃ",romaji:"rya"},{char:"りゅ",romaji:"ryu"},{char:"りょ",romaji:"ryo"},{char:"ぎゃ",romaji:"gya"},{char:"ぎゅ",romaji:"gyu"},{char:"ぎょ",romaji:"gyo"},{char:"じゃ",romaji:"jya"},{char:"じゅ",romaji:"jyu"},{char:"じょ",romaji:"jyo"},{char:"びゃ",romaji:"bya"},{char:"びゅ",romaji:"byu"},{char:"びょ",romaji:"byo"},{char:"ぴゃ",romaji:"pya"},{char:"ぴゅ",romaji:"pyu"},{char:"ぴょ",romaji:"pyo"}];
-const doubleRomaji = [
-    { hiragana: "しゃ", hepburn: "sha", kunrei: "sya" },
-    { hiragana: "しゅ", hepburn: "shu", kunrei: "syu" },
-    { hiragana: "しょ", hepburn: "sho", kunrei: "syo" },
 
-    { hiragana: "ちゃ", hepburn: "cha", kunrei: "cya" },
-    { hiragana: "ちゅ", hepburn: "chu", kunrei: "cyu" },
-    { hiragana: "ちょ", hepburn: "cho", kunrei: "cyo" },
+let loadMainKana = true;
+let loadDakutenKana = true;
+let loadCombinationKana = true;
 
-    { hiragana: "じゃ", hepburn: "ja", kunrei: "zya" },
-    { hiragana: "じゅ", hepburn: "ju", kunrei: "zyu" },
-    { hiragana: "じょ", hepburn: "jo", kunrei: "zyo" },
-
-    { hiragana: "つゃ", hepburn: "tsa", kunrei: "tya" },
-    { hiragana: "つゅ", hepburn: "tsyu", kunrei: "tyu" },
-    { hiragana: "つょ", hepburn: "tso", kunrei: "tyo" },
-
-    { hiragana: "ふゃ", hepburn: "fya", kunrei: "hya" },
-    { hiragana: "ふゅ", hepburn: "fyu", kunrei: "hyu" },
-    { hiragana: "ふょ", hepburn: "fyo", kunrei: "hyo" }
-];
+let availableHiragana= [];
+let doubleRomaji = [];
 
 
+async function loadJSON() {
+    try {
+        const response = await fetch('hiragana/hiragana.json');
+        if (!response.ok) {
+            console.log('Network response was not ok');
+            return;
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error loading JSON: ', error, "\nIf this persists, please contact the website administrator.");
+        throw error;
+    }
+}
 
-let remainingHiragana = [...availableHiragana];
+let jsonContent;
+let jsonContentLoaded = false;
+
+loadJSON().then(jsonContent => {
+    if(jsonContent){
+        if (loadMainKana) {
+            availableHiragana.push(...jsonContent["single_hiragana"]);
+            availableHiragana.push(...jsonContent["dakuten_hiragana"]);
+        }
+        if(loadDakutenKana){
+            availableHiragana.push(...jsonContent["dakuten_hiragana"]);
+        }
+        if(loadCombinationKana){
+            availableHiragana.push(...jsonContent["combo_hiragana"]);
+        }
+        jsonContentLoaded = true;
+        remainingHiragana = availableHiragana;
+        loadNextCard();
+    }
+}).catch(error => {
+    console.error("Error parsing JSON into Array:", error);
+});
+
+
+let remainingHiragana = availableHiragana;
 
 let currentStreak = 0;
 let startTime;
@@ -101,6 +122,7 @@ function loadNextCard() {
     fadeElement(cardsContainer)
     switchCheckButton(false);
 
+    console.log(remainingHiragana.length)
     if (remainingHiragana.length > 0) {
         currentCardIndex = Math.floor(Math.random() * remainingHiragana.length);
         hiraganaText.textContent = remainingHiragana[currentCardIndex].char;
@@ -110,7 +132,7 @@ function loadNextCard() {
 
         startTime = new Date();
 
-    } else {
+    } else if(jsonContentLoaded) {
         showCompletionScreen();
     }
 }
@@ -149,15 +171,19 @@ inputField.addEventListener("keyup", function(event) {
 function handleAnswer(){
 
     const userAnswer = inputField.value.toLowerCase().trim()
-    const correctAnswer = remainingHiragana[currentCardIndex]?.romaji;
+    const correctAnswer = remainingHiragana[currentCardIndex]?.["romaji"];
+
+    console.log(remainingHiragana[1]);
+    console.log(currentCardIndex);
+
     const currentHiragana = remainingHiragana[currentCardIndex].char;
 
     let isAnswerCorrect = userAnswer === correctAnswer || currentHiragana === "じ" && (userAnswer === "ji" || userAnswer === "zi");
 
     for (let i = 0; i < doubleRomaji.length; i++) {
-        if (doubleRomaji[i].hiragana === currentHiragana) {
+        if (doubleRomaji[i]["hiragana"] === currentHiragana) {
             console.log("Is here");
-            if(userAnswer === doubleRomaji[i].hepburn || userAnswer === doubleRomaji[i].kunrei){
+            if(userAnswer === doubleRomaji[i]["hepburn"] || userAnswer === doubleRomaji[i]["kunrei"]){
                 console.log("Is true");
                 isAnswerCorrect = true;
                 break;
@@ -202,4 +228,3 @@ function playPronunciation() {
 
     speechSynthesis.speak(utterance);
 }
-loadNextCard();
